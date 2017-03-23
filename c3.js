293bdb22ffb155e42c4c49be890fac32fdd1a1a7
Dylan Graham <dylan.graham@gmail.com>
2017-03-23 13:37:12 +1100

    flatmap/filter

--- c3.js
+++ c3.js
@@ -4,3 +4,10 @@ const addSixPercent = x => x + (x * .06);
 Rx.Observable.of(10.0, 20.0, 30.0, 40.0)
     .map(addSixPercent)
     .subscribe(x => console.log(x));
+
+Rx.Observable.from([
+    'The quick brown fox',
+    'jumps over the lazy dog'])
+    .flatMap(str => str.split(' '))
+    .filter(str => str.includes('o'))
+    .subscribe(x => console.log(x));
