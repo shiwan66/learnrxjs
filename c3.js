47fce424cedcf3154f72299558e33e56ee5e88d5
Dylan Graham <dylan.graham@gmail.com>
2017-03-23 16:03:17 +1100

    pluck

--- c3.js
+++ c3.js
@@ -1,6 +1,7 @@
 const Rx = require('rxjs/Rx');
 
 const addSixPercent = x => x + (x * .06);
+
 Rx.Observable.of(10.0, 20.0, 30.0, 40.0)
     .map(addSixPercent)
     .subscribe(x => console.log(x));
@@ -15,3 +16,17 @@ Rx.Observable.from([
         return acc;
     }, "")
     .subscribe(x => console.log(x));
+
+let candidates = [
+    {name: 'Brendan Eich', experience: 'JavaScript Inventor', testing: { nest: true }},
+    {name: 'Emmet Brown', experience: 'Historian', testing: { nest: false }},
+    {name: 'George Lucas', experience: 'Sci-fi writer', testing: { nest: true }},
+    {name: 'Alberto Perez', experience: 'Zumba Instructor', testing: { nest: true }},
+    {name: 'Bjarne Stroustrup', experience: 'C++ Developer', testing: { nest: true }}
+];
+
+Rx.Observable.from(candidates)
+    .pluck('testing', 'nest')
+    .take(2)
+    .do(name => console.log(name))
+    .subscribe();
