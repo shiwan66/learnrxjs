5c6a8884c33e03b9bc67deacc62437171c9f0d9e
Dylan Graham <dylan.graham@gmail.com>
2017-03-27 10:20:42 +1100

    correct Money object

--- c4-1.js
+++ c4-1.js
@@ -1,10 +1,17 @@
-const Rx = require('rxjs/Rx');
+// const Rx = require('rxjs/Rx');
 
-const Money = (currency, val) => ({
-    value: () => val,
-    currency: () => currency,
-    toString: () => `${currency} ${val}`
-});
+const Money = function (currency, val) {
+    return {
+        value: () => val,
+        currency: () => currency,
+        toString: () => `${currency} ${val}`
+    }
+};
 
 const newRandomNumber = () => Math.floor(Math.random() * 100);
-console.log(newRandomNumber());
+
+Rx.Observable.interval(2000)
+    .skip(1)
+    .take(5)
+    .map(num => new Money('AUD', newRandomNumber()))
+    .subscribe(price => document.querySelector('#price').textContent = price);
