ba0294adabe431696b2e2c931d33abb079ec5553
Dylan Graham <dylan.graham@gmail.com>
2017-03-27 10:44:34 +1100

    click debounce

--- c4-1.js
+++ c4-1.js
@@ -15,3 +15,7 @@ Rx.Observable.interval(2000)
     .take(5)
     .map(num => new Money('AUD', newRandomNumber()))
     .subscribe(price => document.querySelector('#price').textContent = price);
+
+Rx.Observable.fromEvent(document, 'click')
+    .debounceTime(1000)
+    .subscribe(c => console.log(`Clicked at position ${c.clientX} and ${c.clientY}`));
