b15793dc3f0fceb88e48490432219cbd9e8a39b2
Dylan Graham <dylan.graham@gmail.com>
2017-03-28 11:54:20 +1100

    bufferWhen

---
+++ c4-5.js
@@ -0,0 +1,23 @@
+window.onload = function () {
+    const field = document.querySelector('.form-field');
+    const showHistoryButton = document.querySelector('#show-history');
+    const historyPanel = document.querySelector('#history');
+
+    const showHistory$ = Rx.Observable.fromEvent(showHistoryButton, 'click');
+
+    Rx.Observable.fromEvent(field, 'keyup')
+        .debounceTime(700)
+        .pluck('target', 'value')
+        .filter(R.compose(R.not, R.isEmpty)) //#A
+        .bufferWhen(() => showHistory$) //#B
+        .do(history => history.pop())
+        .subscribe(history => {
+            let contents = '';
+            if (history.length > 0) { //#C
+                for (let item of history) {
+                    contents += '<li>' + item + '</li>';
+                }
+                historyPanel.innerHTML = contents;
+            }
+        });
+};
