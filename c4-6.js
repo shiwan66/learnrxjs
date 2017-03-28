a7207f51a88ac53a6288e54f5d69e49c57dd5e06
Dylan Graham <dylan.graham@gmail.com>
2017-03-28 14:52:15 +1100

    combineLatest

---
+++ c4-6.js
@@ -0,0 +1,25 @@
+window.onload = function () {
+    const password = document.getElementById('password-field');
+    const submit = document.getElementById('submit');
+    const outputField = document.getElementById('output');
+
+    const password$ = Rx.Observable.fromEvent(password, 'keyup')
+        .map(({keyCode}) => keyCode - 48);
+
+    const submit$ = Rx.Observable.fromEvent(submit, 'click');
+
+    Rx.Observable.combineLatest(
+        password$.bufferTime(7000).filter(R.compose(R.not, R.isEmpty)), submit$)
+        .take(10)
+        .do(([maybePassword,]) => console.log('Password is: ' + maybePassword.join('-')))
+        .subscribe(
+            ([maybePassword,]) => {
+                if (maybePassword.join('') === '1337') { //#C
+                    outputField.innerHTML = 'Correct Password!';
+                } else {
+                    outputField.innerHTML = 'Wrong Password!';
+                }
+            },
+            null,
+            () => outputField.innerHTML += '\n No more tries accepted!');
+};
