a55dfffcd82213fa2ca63df1e2cfe2e8fdc55d46
Dylan Graham <dylan.graham@gmail.com>
2017-03-28 15:27:01 +1100

    make a nice material style card for github project

--- c4.js
+++ c4.js
@@ -1,24 +1,34 @@
-const Rx = require('rxjs/Rx');
-const axios = require('axios');
+window.onload = function () {
+    const url = 'https://api.github.com/users/JakeWharton/starred';
+    const cardimage = document.querySelector("#cardimage");
+    const cardtitle = document.querySelector("#cardtitle");
+    const cardcontent = document.querySelector("#cardcontent");
+    const cardlink = document.querySelector("#cardlink");
 
-const url = 'https://api.github.com/users/JakeWharton/starred';
-
-Rx.Observable.fromPromise(axios.get(url))
-    .map(response => response.data)
-    .concatAll()
-    .take(2)
-    .map(data => {
-        return {
-            avatar: data.owner.avatar_url,
-            desc: data.description,
-            forks: data.forks,
-            login: data.owner.login,
-            name: data.name,
-            stars: data.stargazers_count,
-            url: data.html_url
-        }
-    })
-    .subscribe(
-        response => console.log(response),
-        error => console.log("Error occurred: ", error)
-    );
+    Rx.Observable.fromPromise(axios.get(url))
+        .map(response => response.data)
+        .concatAll()
+        .take(1)
+        .map(data => {
+            return {
+                avatar: data.owner.avatar_url,
+                desc: data.description,
+                forks: data.forks,
+                login: data.owner.login,
+                name: data.name,
+                stars: data.stargazers_count,
+                url: data.html_url
+            }
+        })
+        .subscribe(
+            response => {
+                console.log(response);
+                cardimage.src = response.avatar;
+                cardtitle.innerHTML = response.name;
+                cardcontent.innerHTML = response.desc;
+                cardlink.href = response.url;
+                cardlink.innerHTML = response.login + " - " + response.name;
+            },
+            error => console.log("Error occurred: ", error)
+        );
+};
