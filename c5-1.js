7e2063364152b0748b3e82a9b8b1559955af6e0a
Dylan Graham <dylan.graham@gmail.com>
2017-03-29 16:54:18 +1100

    map each event to X, Y coordinates

--- c5-1.js
+++ c5-1.js
@@ -1,8 +1,16 @@
 const click1 = document.getElementById('click1');
 const click2 = document.getElementById('click2');
 
-const c1$ = Rx.Observable.fromEvent(click1, 'click');
-const c2$ = Rx.Observable.fromEvent(click2, 'click');
+const c1$ = Rx.Observable.fromEvent(click1, 'click')
+    .map(event => ({
+        x: event.pageX,
+        y: event.pageY
+    }));
+const c2$ = Rx.Observable.fromEvent(click2, 'click')
+    .map(event => ({
+        x: event.pageX,
+        y: event.pageY
+    }));
 
 const combined = Rx.Observable.merge(c1$, c2$)
     .subscribe(e => console.log(e));
