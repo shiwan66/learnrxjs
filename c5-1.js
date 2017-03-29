054c5ef9ba7e153759aebb11197da83ef41fc0c1
Dylan Graham <dylan.graham@gmail.com>
2017-03-29 15:25:06 +1100

    relocate scripts

--- c5-1.js
+++ c5-1.js
@@ -1,10 +1,8 @@
-window.onload = function () {
-    const click1 = document.getElementById('click1');
-    const click2 = document.getElementById('click2');
+const click1 = document.getElementById('click1');
+const click2 = document.getElementById('click2');
 
-    const c1$ = Rx.Observable.fromEvent(click1, 'click');
-    const c2$ = Rx.Observable.fromEvent(click2, 'click');
+const c1$ = Rx.Observable.fromEvent(click1, 'click');
+const c2$ = Rx.Observable.fromEvent(click2, 'click');
 
-    const combined = Rx.Observable.merge(c1$, c2$)
-        .subscribe(e => console.log(e));
-};
+const combined = Rx.Observable.merge(c1$, c2$)
+    .subscribe(e => console.log(e));
